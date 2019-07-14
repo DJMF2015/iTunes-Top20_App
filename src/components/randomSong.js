931291759490341ed0a53ce4947618281d7a02e2
DJMF <djm.fulton@gmail.com>
2019-07-14 14:59:39 +0100

    Update randomSong.js

--- src/components/randomSong.js
+++ src/components/randomSong.js
@@ -1,1 +1,16 @@
 import React, {Fragment} from 'react';
+  selectedRandomSong(song){
+         let currentIndex = song.length, temporaryValue, randomIndex;
+
+         while(0 !== currentIndex){
+             randomIndex = Math.floor(Math.random()*currentIndex);
+             currentIndex -= 1;
+
+
+         temporaryValue = song[currentIndex];
+         song[currentIndex] = song[randomIndex];
+         song[randomIndex] = temporaryValue;
+         }
+         return song;
+     }
+
